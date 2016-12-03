6a3c1cee1980f70dd8dfe89822419f7cfe9182c6
LinWeiHan <shps951002@gmail.com>
2016-12-03 14:49:15 +0800

    ServletContext跟HttpSession之間的存活時間，可見度

--- src/ServletContext_Set.java
+++ src/ServletContext_Set.java
@@ -2,7 +2,17 @@
 import java.io.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
-
+/*
+ * 要先運行Get，才能放Set
+ * 
+ * O存活時間ServletContent > Session
+ *   content除非server關必要不然會保存資料
+ *   session關閉瀏覽器，資料就會刪除。(or 存活三十分鐘)
+ * O可見度ServletContent > Session:
+ * 	content 兩個瀏覽器之間可以看到
+ * 	session 不可以 
+ * 
+ */
 public class ServletContext_Set extends HttpServlet {
 
 
