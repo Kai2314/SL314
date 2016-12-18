ca18ebb498f7bb50251f731259ea51a215300d98
LinWeiHan <shps951002@gmail.com>
2016-12-18 23:11:52 +0800

    chatroom

--- src/servlet_examples/HelloWorld.java
+++ src/servlet_examples/HelloWorld.java
@@ -9,20 +9,20 @@ public class HelloWorld extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		res.setContentType("text/html; charset=Utf-8");
 		PrintWriter out = res.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println("<BIG>Hello World , 世界你好23 !</BIG>");
-		out.println("</BODY></HTML>");	
+		// out.println("<HTML>");
+		// out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
+		// out.println("<BODY>");
+		// out.println("<BIG>GET</BIG>");
+		// out.println("</BODY></HTML>");
 	}
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-		res.setContentType("text/html; charset=Utf-8");
-		PrintWriter out = res.getWriter();
-		out.println("<HTML>");
-		out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
-		out.println("<BODY>");
-		out.println("<BIG>Hello World , 世界你好 !</BIG>");
-		out.println("</BODY></HTML>");
+		 res.setContentType("text/html; charset=Utf-8");
+		 PrintWriter out = res.getWriter();
+		// out.println("<HTML>");
+		// out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
+		// out.println("<BODY>");
+		// out.println("<BIG>POST</BIG>");
+		// out.println("</BODY></HTML>");
 	}
 }
